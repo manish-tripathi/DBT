1724dcb35110a1d85636fda579274a28d539bbba
Manish Tripathi <maneeshtripathi@gmail.com>
2024-10-20 18:03:53 +0000

    Updated

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,26 +1,30 @@
-with orders as (
-    select order_id,
-    customer_id
-    from {{ ref("stg_jaffle_shop__orders") }}
-    ),
-
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
 
 payments as (
-    select orderid as order_id,
-    amount
-    from {{ ref("stg_stripe__payments") }}
-    ),
+    select * from {{ ref ('stg_stripe__payments') }}
+),
 
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
 
-final as (
-    select o.*,
-    p.amount
-    from orders o left join payments p using (order_id)
-    )
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
 
 select * from final
-
-
-
-
-
